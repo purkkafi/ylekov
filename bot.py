3164abe5b317c0aefb93e6a3d19f7afee628e08f
mikkotampio <tampio.mikko@gmail.com>
2017-06-13 22:31:05 +0300

    Added bot code

---
+++ bot.py
@@ -0,0 +1,25 @@
+import tweepy
+import subprocess
+import sys
+from time import sleep
+from secrets import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret);
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+def post():
+	subprocess.run(["java", "-jar", "ylekov.jar", "update"])
+	result = subprocess.run(["java", "-jar", "ylekov.jar", "generate"], stdout=subprocess.PIPE)
+	tweet = result.stdout.decode("utf-8").strip()
+	
+	api.update_status(tweet)
+
+while True:
+	try:
+		post()
+		sleep(60*60)
+	except:
+		print("Error: ", sys.exc_info()[0])
+		sleep(60)
