cfccb27a04b6a84a956f702f02f70f857982be08
mikkotampio <tampio.mikko@gmail.com>
2021-01-19 00:49:41 +0200

    implemented ylekov classic

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import tweepy
 import telegram
 import subprocess
 import sys
+import random
 from datetime import datetime
 from time import sleep
 from secrets import *
@@ -15,55 +16,61 @@ api = tweepy.API(auth)
 bot = telegram.Bot(telegram_token)
 
 def update_cache():
-	subprocess.Popen(["java", "-jar", "ylekov.jar", "update"]).wait()
+    subprocess.Popen(["java", "-jar", "ylekov.jar", "update"]).wait()
 
 def get_post():
-	result = subprocess.Popen(["java", "-jar", "ylekov.jar", "generate"], stdout=subprocess.PIPE).communicate()
-	tweet = result[0].decode("utf-8").strip()
-	return tweet
-	
+    if datetime.now().hour == 18: # ylekov classic
+        with open("ylekov_classic.txt") as f:
+            urls = f.read().split('\n')
+        url = random.choice(urls)
+        return "klassinen ylekov " + url
+        
+    else: # tavallinen ylekov
+        result = subprocess.Popen(["java", "-jar", "ylekov.jar", "generate"], stdout=subprocess.PIPE).communicate()
+        tweet = result[0].decode("utf-8").strip()
+        return tweet
+
 def post_twitter(post):
-	if is_debug:
-		print("[TWITTER] " + post)
-	else:
-		api.update_status(post)
+    if is_debug:
+        print("[TWITTER] " + post)
+    else:
+        api.update_status(post)
 
 def post_telegram(post):
-	if is_debug:
-		print("[TELEGRAM] " + post)
-	else:
-		bot.send_message(telegram_channel, post, disable_web_page_preview=True)
+    if is_debug:
+        print("[TELEGRAM] " + post)
+    else:
+        bot.send_message(telegram_channel, post, disable_web_page_preview=True)
 
 def log(message):
-	with open("ylekov_log", "a") as file:
-		file.write("[" + str(datetime.now()) + "] " + message + "\n")
+    with open("ylekov_log", "a") as file:
+        file.write("[" + str(datetime.now()) + "] " + message + "\n")
 
 def try_and_log(callback, description):
-	try:
-		return_value = callback()
-		log("Completed action [" + description + "]")
-		return return_value
-	except:
-		log("Failed action [" + description + "]: " + str(sys.exc_info()[:2]))
+    try:
+        return_value = callback()
+        log("Completed action [" + description + "]")
+        return return_value
+    except:
+        log("Failed action [" + description + "]: " + str(sys.exc_info()[:2]))
 
 def runbot():
-	while True:
-	
-		try_and_log(lambda: update_cache(), "update cache")
-		post = try_and_log(lambda: get_post(), "generate post")
-		
-		try_and_log(lambda: post_twitter(post), "post to Twitter: " + post)
-		try_and_log(lambda: post_telegram(post), "post to Telegram: " + post)
-		
-		sleep(60*60)
+    while True:
+        try_and_log(lambda: update_cache(), "update cache")
+        post = try_and_log(lambda: get_post(), "generate post")
+        
+        try_and_log(lambda: post_twitter(post), "post to Twitter: " + post)
+        try_and_log(lambda: post_telegram(post), "post to Telegram: " + post)
+        
+        sleep(60*60)
 
 mode = sys.argv[1]
 
 if mode == "run":
-	is_debug = False
-	runbot()
+    is_debug = False
+    runbot()
 elif mode == "test":
-	is_debug = True
-	runbot()
+    is_debug = True
+    runbot()
 else:
-	print("Invoke with 'run' or 'test'")
+    print("Invoke with 'run' or 'test'")
